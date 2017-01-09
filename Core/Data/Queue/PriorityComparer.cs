3dabd19fc40a58ea0bcceeefe9ab803377591156
Joshua <joshuaavalon@gmail.com>
2017-01-09 16:09:14 +0800

    Update csproj

--- Core/Data/Queue/PriorityComparer.cs
+++ Core/Data/Queue/PriorityComparer.cs
@@ -3,9 +3,7 @@
 namespace AvalonAssets.Core.Data.Queue
 {
     /// <summary>
-    ///     <para>
     ///         Default <see cref="IComparer{T}" /> used by <see cref="PriorityQueue{T}" />.
-    ///     </para>
     /// </summary>
     /// <typeparam name="T">Type.</typeparam>
     public class PriorityComparer<T> : IComparer<IPriority<T>>
