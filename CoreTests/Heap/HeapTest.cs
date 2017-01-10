d6c4b8a02f56a89bf83f4446b3aeb339745ee311
Joshua <joshuaavalon@gmail.com>
2017-01-10 11:54:46 +0800

    Update csproj

--- CoreTests/Heap/HeapTest.cs
+++ CoreTests/Heap/HeapTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using AvalonAssets.Core;
 using AvalonAssets.Core.Data.Heap;
 using AvalonAssets.Core.Utility;
 using NUnit.Framework;
